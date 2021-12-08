d99fb2b71a2c002f769c3417b6affb3d56403752
Saravanan, Duruvan <dsaravanan3@gatech.edu>
2021-12-08 17:55:50 -0500

    adding comments to shell script

--- cache/nginx-install.sh
+++ cache/nginx-install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+#install nginx
 sudo apt update
 sudo apt install nginx
+
+#stop nginx
 sudo nginx -s stop
+
+#copy nginx config file
 sudo cp -a nginx.conf /etc/nginx/nginx.conf
+
+#restart nginx
 sudo nginx
 sudo nginx -s reload
+
+#install python requirements
 sudo apt install python-pip
 pip install pandas
